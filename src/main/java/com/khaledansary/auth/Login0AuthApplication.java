abdd8fa23933823da06adfd0115121e44a58b273
khaled-ansary <ansary.development@gmail.com>
2017-03-04 23:12:19 +0100

    maven project with spring, 0Auth, JPA

---
+++ src/main/java/com/khaledansary/auth/Login0AuthApplication.java
@@ -0,0 +1,12 @@
+package com.khaledansary.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login0AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Login0AuthApplication.class, args);
+	}
+}
---
+++ src/test/java/com/khaledansary/auth/Login0AuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khaledansary.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Login0AuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
