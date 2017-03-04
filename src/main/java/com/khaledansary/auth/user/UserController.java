ffa4faba005785ea6ca510a2d6f966c12454d0d8
khaled-ansary <ansary.development@gmail.com>
2017-03-05 00:06:04 +0100

    implement controller to get all users

--- src/main/java/com/khaledansary/auth/user/UserController.java
+++ src/main/java/com/khaledansary/auth/user/UserController.java
@@ -1,5 +1,19 @@
 package com.khaledansary.auth.user;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/users")
+	public List<User> getUsers(){
+		return userService.getUsers();		
+	}
 }
