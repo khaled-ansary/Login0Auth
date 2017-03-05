58d59b6621ac20db2c11396909d779d3c3746983
khaled-ansary <ansary.development@gmail.com>
2017-03-05 17:01:19 +0100

    update as RestController

--- src/main/java/com/khaledansary/auth/Login0AuthApplication.java
+++ src/main/java/com/khaledansary/auth/Login0AuthApplication.java
@@ -2,6 +2,8 @@ package com.khaledansary.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Khaled
@@ -9,8 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@RestController
 public class Login0AuthApplication {
 
+	@RequestMapping("/")
+	String hello(){
+		return "Hello";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Login0AuthApplication.class, args);
 	}
